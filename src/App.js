4a6aa86f1824a9466a674d3a80c3ee368c521944
Dikshit <122359979+DIKSHITS@users.noreply.github.com>
2026-03-11 19:59:14 +0530

    GRS

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
       <Navbar />
       <Hero />
       <About />
-     <Education />
-     <Experience />
+      <Education />
+      <Experience />
       <Skills />
       <Projects />
       <Contact />
